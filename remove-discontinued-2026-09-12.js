f1bfb497d76ae5fecc9504d2289e8567b40f7297
pablomattos777-dev <pablomattos777@gmail.com>
2026-09-14 00:55:00 -0300

    Quitar Si Intense del catalogo

--- remove-discontinued-2026-09-12.js
+++ remove-discontinued-2026-09-12.js
@@ -1,5 +1,5 @@
 (() => {
-  const retirar = new Set(['Hawas Ice', 'Odyssey Homme']);
+  const retirar = new Set(['Hawas Ice', 'Odyssey Homme', 'Sì Intense']);
   for (let i = products.length - 1; i >= 0; i--) {
     if (retirar.has(products[i].name)) products.splice(i, 1);
   }
