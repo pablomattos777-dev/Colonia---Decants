deffdb289926478c6e701d16af11b59bd8d777bf
pablomattos777-dev <pablomattos777@gmail.com>
2026-09-14 01:03:30 -0300

    Agregar cinco perfumes arabes al catalogo

--- new-products-2026-09-12.js
+++ new-products-2026-09-12.js
@@ -2,7 +2,12 @@
   const nuevos = [
     { name: 'Odyssey Homme White Edition', type: 'Árabe', gender: 'Hombre', image: 'images/odyssey-homme-white-edition.jpg' },
     { name: 'Club de Nuit Maleka', type: 'Árabe', gender: 'Mujer', image: 'images/club-de-nuit-maleka.jpg' },
-    { name: 'Nitro Red Dumont', type: 'Árabe', gender: 'Hombre', image: 'images/nitro-red-dumont.jpg' }
+    { name: 'Nitro Red Dumont', type: 'Árabe', gender: 'Hombre', image: 'images/nitro-red-dumont.jpg' },
+    { name: 'Salvo Eau de Parfum', type: 'Árabe', gender: 'Hombre', image: null },
+    { name: 'Salvo Elixir', type: 'Árabe', gender: 'Hombre', image: null },
+    { name: 'Rome Extradose', type: 'Árabe', gender: 'Hombre', image: null },
+    { name: 'Odyssey Aqua', type: 'Árabe', gender: 'Hombre', image: null },
+    { name: 'Yeah! Man Parfum', type: 'Árabe', gender: 'Hombre', image: null }
   ];
 
   nuevos.forEach(item => {
