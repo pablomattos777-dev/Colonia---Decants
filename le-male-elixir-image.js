86d78b5ad306be6b1cb90020ef92b13137d32015
pablomattos777-dev <pablomattos777@gmail.com>
2026-09-16 18:03:11 -0300

    Replace Le Male Elixir image with embedded optimized image

--- le-male-elixir-image.js
+++ le-male-elixir-image.js
@@ -1,5 +1,5 @@
 (() => {
   const p = products.find(x => x.name === 'Le Male Elixir Jean Paul Gaultier');
-  if (p) p.image = 'images/le-male-elixir.jpg';
+  if (p) p.image = 'data:image/webp;base64,UklGRpgBAABXRUJQVlA4WAoAAAAQAAAACwAACwAAQUxQSAwAAAAQUFBRUVFRUVFRUVFRVlA4IGABAABwAQCdASoMAAwAPm0wk0akIyIhKAgAgA2JaQAA3pFqv3L7b/2z8sT/7w8A';
   render();
 })();
