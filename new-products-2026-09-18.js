d7235f55a65023ab94f2b02cfeebc8efe0068f41
pablomattos777-dev <pablomattos777@gmail.com>
2026-09-18 22:07:10 -0300

    Link images to new perfume cards

--- new-products-2026-09-18.js
+++ new-products-2026-09-18.js
@@ -1,12 +1,12 @@
 (() => {
   const nuevos = [
-    { name: "Terre d'Hermès EDT", type: "Diseñador", gender: "Hombre" },
-    { name: "Invictus Elixir Rabanne", type: "Diseñador", gender: "Hombre" }
+    { name: "Terre d'Hermès EDT", type: "Diseñador", gender: "Hombre", image: "images/terre-dhermes-edt.webp" },
+    { name: "Invictus Elixir Rabanne", type: "Diseñador", gender: "Hombre", image: "images/invictus-elixir-rabanne.webp" }
   ];
   nuevos.forEach(item => {
     if (!products.some(p => p.name === item.name)) {
       const nextId = products.reduce((max,p)=>Math.max(max,p.id),0)+1;
-      products.push({ id:nextId, name:item.name, type:item.type, gender:item.gender, prices:priceSets[item.type], image:null });
+      products.push({ id:nextId, name:item.name, type:item.type, gender:item.gender, prices:priceSets[item.type], image:item.image || null });
     }
   });
   render();
