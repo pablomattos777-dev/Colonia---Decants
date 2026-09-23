154e9ce9f8c29dee038a5dce6529e3d839d95c87
pablomattos777-dev <pablomattos777@gmail.com>
2026-09-23 13:20:37 -0300

    Retirar La Vie Est Belle Elixir por falta de stock

--- remove-discontinued-2026-09-12.js
+++ remove-discontinued-2026-09-12.js
@@ -1,5 +1,5 @@
 (() => {
-  const retirar = new Set(['Hawas Ice', 'Odyssey Homme', 'Sì Intense', 'Y Le Parfum']);
+  const retirar = new Set(['Hawas Ice', 'Odyssey Homme', 'Sì Intense', 'Y Le Parfum', 'La Vie Est Belle Elixir']);
   for (let i = products.length - 1; i >= 0; i--) {
     if (retirar.has(products[i].name)) products.splice(i, 1);
   }
